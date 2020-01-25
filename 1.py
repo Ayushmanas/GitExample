24cc48191be1c6979ec9d409fb89ea9a1f354fb5
Ayushmanas <38788102+Ayushmanas@users.noreply.github.com>
2020-01-25 10:11:20 +0530

    First Commit

---
+++ 1.py
@@ -0,0 +1,11 @@
+from csv import reader
+from pprint import pprint
+
+with open("1-dataset.csv") as f:
+    dataset = [row[:-1] for row in reader(f) if row[-1] == "Yes"]
+
+hypothesis = dataset[0][:]
+for row in dataset[1:]:
+    hypothesis = ["?" if tup[0] != tup[1] else tup[0]
+                  for tup in zip(hypothesis, row)]
+print(hypothesis)
