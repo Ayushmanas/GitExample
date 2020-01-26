5bfe3c3a9cf017721f986c9d005612322c387018
ayush <ayush.kr009@gmail.com>
2020-01-26 10:00:09 +0530

    Added a java file

---
+++ javaFiles/BubbleSort.java
@@ -0,0 +1,35 @@
+import java.util.*;
+class BubbleSort{
+
+	public void print(int a[],int n){
+		for(int i=0;i<n;i++){
+			System.out.print(" "+a[i]);
+		}
+		System.out.println();
+	}
+
+	public void sort(int a[],int n){
+		for(int i=0;i<n;i++){
+			for(int j=n-1;i<j;j--){
+				if(a[j-1]>a[j]){
+					int temp=a[j-1];
+					a[j-1]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+	}
+
+	public static void main(String args[]){
+		int n;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		int a[]=new int[n];
+		for(int i=0;i<n;i++){
+			a[i]=sc.nextInt();
+		}
+		BubbleSort b=new BubbleSort();
+		b.sort(a,n);
+		b.print(a,n);
+	}
+}
